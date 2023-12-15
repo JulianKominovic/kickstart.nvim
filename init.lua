ab8ca372544863855ca429cd5a4012add53bc6a0
Julian Kominovic <juliankominovic@gmail.com>
2023-12-15 17:38:45 -0300

    chore: add neo-tree

--- init.lua
+++ init.lua
@@ -108,7 +108,16 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
-
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
